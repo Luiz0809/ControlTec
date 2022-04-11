4e5564f833e28c2cea4d9598159a8c63aed8e996
Willians Ribeiro <willians.filho@bandtec.com.br>
2022-04-11 17:33:10 -0300

    add Dash Versão 0.0.1

---
+++ Dashboard Estático V0.0.1/Dashboard_V0.0.1/charts.js
@@ -0,0 +1,31 @@
+var capacidadeMemoria = document.getElementById('chartMemoria').getContext('2d');
+var chart = new Chart(capacidadeMemoria, {
+    type: 'line',
+    data:{
+        labels: ["Jan", "Fev", "Mar", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
+        datasets: [
+            {
+                label: "Uso da Memória",
+                backgroundColor: '#fff',
+                borderColor: '#f48ec',
+                data: [50, 30, 20, 60, 35, 55, 90, 80, 100],
+            }
+        ]
+    }
+});
+
+var desempenhoCpu = document.getElementById('chartCpu').getContext('2d');
+var chart = new Chart(desempenhoCpu, {
+    type: 'line',
+    data: {
+        labels: ["Jan", "Fev","Abr", "Mar", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
+        datasets: [
+            {
+                label: "% de Utilização",
+                backgroundColor: '#fff',
+                borderColor: '#38d6eb',
+                data: [5000, 3000, 2000, 6000, 3500, 5500, 9000, 8000, 1000],
+            }
+        ]
+    }
+});
