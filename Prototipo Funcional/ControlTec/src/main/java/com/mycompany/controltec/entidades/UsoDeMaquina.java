7da6bb701add1cea2009588a20195c3dfde767e3
devErick <erick.lemosjr@bandtec.com.br>
2022-05-30 17:27:39 -0300

    att jar final 3.0

--- Prototipo Funcional/ControlTec/src/main/java/com/mycompany/controltec/entidades/UsoDeMaquina.java
+++ Prototipo Funcional/ControlTec/src/main/java/com/mycompany/controltec/entidades/UsoDeMaquina.java
@@ -127,7 +127,7 @@ public class UsoDeMaquina {
                     + ", sua máquina está com consumo de CPU acima de 75%"
                     + ", nosso Script de correção será ativado automáticamente");
             for (Processo processo : processos) {
-                if (processo.getUsoCpu() > 75.0) {
+                if (processo.getUsoCpu() > (consumoCPU - 25)) {
                     consumidorDeCPU.add(processo);
                 }
             }
@@ -145,7 +145,7 @@ public class UsoDeMaquina {
         }
 
         for (Processo processo : processos) {
-            if (processo.getUsoMemoria() > 75.0) {
+            if (processo.getUsoMemoria() > (porcentagemMemoria - 25)) {
                 consumidorDeMemoria.add(processo);
             }
         }
